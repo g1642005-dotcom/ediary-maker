b7489c6d15f25c645a15d4e41cd779983c5264e7
g1642005-dotcom <g1642005@gmail.com>
2025-08-27 10:59:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,19 +15,11 @@ generateBtn.addEventListener("click", () => {
 
 // PNGとして保存
 downloadBtn.addEventListener("click", () => {
-    cardPreview.style.border = 'none';
-    imageContainer.style.border = 'none';
-
-    html2canvas(cardPreview, { useCORS: true }).then(canvas => {
+    html2canvas(cardPreview).then(canvas => {
         const link = document.createElement("a");
         link.download = "ediary.png";
         link.href = canvas.toDataURL();
         link.click();
-
-        setTimeout(() => {
-            cardPreview.style.border = '1px solid #ddd';
-            imageContainer.style.border = '2px dashed #49a67c';
-        }, 300);
     });
 });
 
@@ -39,68 +31,5 @@ templateIcons.forEach(icon => {
 
         templateIcons.forEach(i => i.classList.remove("active"));
         icon.classList.add("active");
-
-        textContainer.style.top = '0';
-        textContainer.style.left = '0';
-        textContainer.style.width = '100%';
-        textContainer.style.height = '100%';
-        imageContainer.style.display = 'none';
-
-        if (bgImage.includes("-img.png")) {
-            imageContainer.style.display = 'block';
-
-            switch (bgImage) {
-                case "images/background1-img.png":
-                    textContainer.style.top = '50%';
-                    textContainer.style.left = '5%';
-                    textContainer.style.width = '45%';
-                    textContainer.style.height = '40%';
-                    imageContainer.style.top = '10%';
-                    imageContainer.style.left = '55%';
-                    imageContainer.style.width = '40%';
-                    imageContainer.style.height = '40%';
-                    break;
-                case "images/background2-img.png":
-                    textContainer.style.top = '55%';
-                    textContainer.style.left = '10%';
-                    textContainer.style.width = '80%';
-                    textContainer.style.height = '35%';
-                    imageContainer.style.top = '10%';
-                    imageContainer.style.left = '10%';
-                    imageContainer.style.width = '80%';
-                    imageContainer.style.height = '40%';
-                    break;
-                case "images/background3-img.png":
-                    textContainer.style.top = '55%';
-                    textContainer.style.left = '10%';
-                    textContainer.style.width = '80%';
-                    textContainer.style.height = '35%';
-                    imageContainer.style.top = '10%';
-                    imageContainer.style.left = '10%';
-                    imageContainer.style.width = '80%';
-                    imageContainer.style.height = '40%';
-                    break;
-                case "images/background4-img.png":
-                    textContainer.style.top = '40%';
-                    textContainer.style.left = '5%';
-                    textContainer.style.width = '45%';
-                    textContainer.style.height = '50%';
-                    imageContainer.style.top = '5%';
-                    imageContainer.style.left = '50%';
-                    imageContainer.style.width = '45%';
-                    imageContainer.style.height = '30%';
-                    break;
-                default:
-                    textContainer.style.top = '20%';
-                    textContainer.style.left = '10%';
-                    textContainer.style.width = '80%';
-                    textContainer.style.height = '60%';
-                    break;
-            }
-        }
     });
 });
-
-if (templateIcons.length > 0) {
-    templateIcons[0].classList.add("active");
-}
